62fee683a02480c3dc0b89f9aac363fcffc6b5dc
Splurge2001 <2587403986@qq.com>
2025-08-22 17:11:19 +0800

    heng

--- src/rams_interface/src/main_window.cpp
+++ src/rams_interface/src/main_window.cpp
@@ -57,3 +57,47 @@ MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent) {
 
 void MainWindow::cloudCallback(const sensor_msgs::msg::PointCloud2::SharedPtr) {
   // no-op, subscription maintains connection
+}
+
+void MainWindow::handlePose(const geometry_msgs::msg::PoseStamped &pose) {
+  auto request =
+      std::make_shared<rams_interface::srv::MoveToPose::Request>();
+  request->target = pose;
+  auto future = move_client_->async_send_request(request);
+  if (future.wait_for(std::chrono::seconds(5)) ==
+      std::future_status::ready) {
+    auto response = future.get();
+    QString msg = QString::fromStdString(response->message);
+    if (response->success) {
+      QMessageBox::information(this, "MoveToPose", msg);
+    } else {
+      QMessageBox::warning(this, "MoveToPose", msg);
+    }
+  } else {
+    QMessageBox::warning(this, "MoveToPose", "Service call failed");
+  }
+}
+
+bool MainWindow::eventFilter(QObject *obj, QEvent *event) {
+  if (obj == render_panel_ && event->type() == QEvent::MouseButtonPress) {
+    QMouseEvent *mouse = static_cast<QMouseEvent *>(event);
+    Ogre::Vector3 pos;
+    auto sel = viz_manager_->getSelectionManager();
+    Ogre::Viewport *viewport =
+        render_panel_->getRenderWindow()->get_render_window()->getViewport(0);
+    if (sel->get3DPoint(viewport, mouse->x(), mouse->y(), pos)) {
+      geometry_msgs::msg::PoseStamped ps;
+      ps.header.frame_id = viz_manager_->getFixedFrame().toStdString();
+      ps.header.stamp = node_->now();
+      ps.pose.position.x = pos.x;
+      ps.pose.position.y = pos.y;
+      ps.pose.position.z = pos.z;
+      ps.pose.orientation.w = 1.0;
+      handlePose(ps);
+    }
+    return true;
+  }
+  return QMainWindow::eventFilter(obj, event);
+}
+
+} // namespace rams_interface
